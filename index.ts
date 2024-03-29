31e3f84005b4315659af328b0c472012bb820823
ArabkhonDev <starc9899@gmail.com>
2024-03-29 20:58:34 +0300

    change main file

---
+++ index.ts
@@ -0,0 +1,24 @@
+/* data types 
+number
+boolean
+string
+bigint
+symbol
+*/
+// text type
+let firstName: string = "Dylan";
+console.log(firstName);
+
+// number type 
+let age:number = 15;
+console.log(age);
+
+// float type
+
+let ball:boolean = true;
+console.log(ball);
+
+// bigint type 
+let bigNumber: bigint = 15;
+console.log(bigNumber);
+
